9bfbc149cf3c9f99a4414d35ea1f2cd2aa855e1b
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2022-04-12 00:54:13 +0200

    Add visual tracing of best route

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -7,29 +7,25 @@ function Grid(props) {
         var size = 3;
         return [id - size, id + size, id - 1, id + 1].filter(x => x >= 0 && x < props.numberOfCells);
     };
-    // const cells = [...Array(props.numberOfCells).keys()].map(x =>
-    //     <div>
-    //         {x}:{getNeighbors(x)}
-    //     </div>
 
-    // )
+    const [route, setRoute] = useState([]);
+
     const cells = [...Array(props.numberOfCells).keys()].map(x => {
         let neighbors = getNeighbors(x);
         let weights = neighbors.map(x => x);
-        return <GridCell id={x} neighbors={neighbors} weights={weights} />
+        return <GridCell id={x} neighbors={neighbors} weights={weights} className={route.includes(x) ? "grid-cell visited" : "grid-cell"} />
     });
 
     const Dijkstra = (source) => {
         let d = new Array(cells.length);
         let p = new Array(cells.length);
         let q = [];
-        cells.map(x => {
+        cells.forEach(x => {
             d[x.props.id] = Number.MAX_VALUE;
             p[x.props.id] = -1;
             q.push(x);
         });
         d[source] = 0;
-        console.log("i'm here")
 
         while (q.length) {
             q.sort((a, b) => d[b.props.id] - d[a.props.id]);
@@ -43,14 +39,28 @@ function Grid(props) {
                 }
             });
         }
-        console.log(p)
-        return d, p;
+        return [d, p];
     };
+    const getRoute = (end, p) => {
+        let x = p[end];
+        let result = [end];
+        while (x !== -1) {
+            result.push(x);
+            x = p[x];
+        }
+        setRoute(result);
+    };
+    useEffect(()=>{
+        // do stuff here...
+        let p = Dijkstra(0)[1];
+        getRoute(9, p);
+    }, []) // <-- empty dependency array
     return (
         <div className="container">
       {cells}
       {cells[0].props.id}
-      {Dijkstra(0)[1]}
+      {/* {Dijkstra(0)} */}
+      
     </div>
     );
 }
