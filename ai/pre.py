ab3da291d2106ba4ac0bfa0c5f357575e1f54cfd
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2022-04-17 12:23:21 +0200

    Add Food101 transfer-learned ResNet model with basic TorchServe requirements

---
+++ ai/pre.py
@@ -0,0 +1,21 @@
+
+from torchvision import models
+
+model = models.resnet18(pretrained=True)
+model.eval()
+
+for param in model.parameters():
+    param.requires_grad = False
+
+
+import torch.nn as nn
+
+sequential_layer = nn.Sequential(
+    nn.Linear(model.fc.in_features, 128),
+    nn.ReLU(),
+    nn.Dropout(.2),
+    nn.Linear(128, 10),
+    nn.LogSoftmax(dim=1)
+)
+
+model.fc = sequential_layer
