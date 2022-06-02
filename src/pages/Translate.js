ac4d1d9205e591504fc9a0a644ae1eb210e6f842
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2022-06-03 00:34:21 +0200

    Limit spell check POST request to only when word count changes

--- src/pages/Translate.js
+++ src/pages/Translate.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 
 import Grid from "../components/Grid";
@@ -10,8 +10,10 @@ import Corrections from '../components/Corrections';
 
 function Translate() {
   const [currentTime, setCurrentTime] = useState(0);
+  const [currentText, setCurrentText] = useState("a test sentence");
   const [corrections, setCorrections] = useState(
     {
+
       sure: ["sures", "her"],  
       war: ["born", "car"],  
     }
@@ -20,7 +22,7 @@ function Translate() {
   //   sure: ["sures", "her"],  
   //   war: ["born", "car"],  
   // }
-
+  var wordCountAtLastCheck = useRef(null);
   useEffect(() => {
     axios.get('/time')
     .then(response => {
@@ -33,7 +35,7 @@ function Translate() {
     axios({
       method: "post",
       url: url,
-      data: tst,
+      data: currentText,
       headers: {
         // "Content-Type": "image/jpeg",//"multipart/form-data",
         "Content-Type": "text",
@@ -46,12 +48,29 @@ function Translate() {
 
   // The second parameter defines what states this depends on. 
   // This means only initially and not specifying means depend on every state (i.e also setCurrentTime)
-  }, []);
+  }, [currentText]);
 
   const handleTextChange = (event) => {
       // let text = event.currentTarget.textContelengthnt;
       let text = event.currentTarget.value;
       console.log("New Text:", text);
+    
+    
+      // Regular expresions we will use
+    var regex = /\b(\w+)\b/g;
+    var matches = [];
+    // while(word = regex.pause.exec(text)) {
+    //   matches.push(word);
+    // };
+
+    matches = text.match(regex);
+    if (wordCountAtLastCheck.current !== matches.length) {
+      console.log("Setting current text..", matches.length, wordCountAtLastCheck.current);
+      setCurrentText(text);
+      wordCountAtLastCheck.current = matches.length;
+    }
+    console.log(wordCountAtLastCheck.current);
+    console.log("Matched", matches);
   }
 
   return (
