07b66e86cc1d6aed32e801ae68a19caf9804dccc
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2022-06-10 11:55:02 +0200

    Integrate trie algorithm with translate class

--- translator/code/translate.py
+++ translator/code/translate.py
@@ -5,9 +5,13 @@ import unicodedata
 import difflib
 import re
 class Translate:
-    def __init__(self):
+    def __init__(self, spellchecker_corpus=[]):
         self.translator_nl_en = GoogleTranslator(source='nl', target='en')
-        self.translator_en_nl = GoogleTranslator(source='en', target='nl') 
+        self.translator_en_nl = GoogleTranslator(source='en', target='nl')
+
+        if len(spellchecker_corpus) > 0:
+            self.trie = Trie()
+            self.trie.populate(spellchecker_corpus)
 
     @staticmethod
     def distanceToWord(hide, seek):
@@ -173,11 +177,105 @@ class Translate:
             if words_corpus["closest"].iloc[0] > 0:
                 translations[word] = words_corpus.head(3)["pap-simple"].to_list()
         return translations
+    
+    def getFastWordCorrections(self, sentence, check_alternatives=False):
+        translations = {}   
+
+        for word in sentence.split():
+            word = word.lower()
+            exists = self.trie.find(word)
+            if not exists and check_alternatives:
+                accented_match = self.trie.lenientFind(word)
+                if accented_match:
+                    translations[word] = accented_match
+        return translations
         
     def correctSentence(self, sentence):
         nl_en = self.translator_nl_en.translate(sentence)
         en_nl = self.translator_en_nl.translate(nl_en)
         return en_nl
+class Trie:
+    def __init__(self, lastNode=False):
+        self.children = {}
+        self.lastNode = lastNode
+        self.accented = {
+            "a" : ["à","á"], 
+            "e" : ["è","é"], 
+            "i" : ["ì","í"], 
+            "o" : ["ò","ó"], 
+            "u" : ["ù","ú"], 
+            "n" : ["ñ"], 
+        }
+    def __str__(self):
+        if self.children:
+            ret = f"{len(self.children)}"
+            for k, v in self.children.items():
+                ret += f" {k}: {v.lastNode} {v}"
+            return ret
+        else:
+            return ""
+
+    def insert(self, word):
+        current = self
+        for i, l in enumerate(word):
+            if l in current.children:
+                # print(f"from {current} -> ")
+                current = current.children[l]
+                # print(f"to {current}")
+            else:
+                newNode = Trie()
+                # print(f"inserting {l} into {current}")
+                current.children[l] = newNode
+                # print(f"new {len(current.children)} children {current}")
+                current = newNode
+            if i == len(word)-1:
+                current.lastNode = True
+
+    def find(self, word):
+        current = self
+        for i, l in enumerate(word):
+            if l in current.children:
+                current = current.children[l]
+                if i == len(word)-1:
+                    return current.lastNode
+            else:
+                return False
+
+    def lenientFind(self, word):
+        found_word = "" 
+        current = self
+        for i, l in enumerate(word):
+            found_key = False
+            # print(f"checking if {l} has accented")
+            if l in self.accented:
+                for accented_l in self.accented[l]:
+                    # print(f"checking {accented_l}")
+                    if accented_l in current.children:
+                        # print(f"found {accented_l}")
+                        accented_search = accented_l + word[i+1:]
+                        found_accented_word = current.lenientFind(accented_search)
+                        if found_accented_word:
+                            return found_word + found_accented_word
+                
+            if l in current.children:
+                current = current.children[l]
+                found_word += l
+                found_key = True
+
+            if not found_key:
+                return ""
+            
+            if i == len(word)-1:
+                if current.lastNode:
+                    return found_word
+                else:
+                    return ""
+            
+    
+    def populate(self, words):
+        for word in words:
+            self.insert(word)
+
 if __name__ == "__main__":
     # print(Translate.remove_tags("fef [fe] (ef)", ["[", "("]))
     print(Translate.find_between_tags("fef [ef) [fe) (ef[", ["[", "("]))
