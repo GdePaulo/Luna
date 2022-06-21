bf0afa9a2aed892bd96305893f36a82031d858bd
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2022-06-21 22:07:04 +0200

    Add go to feed button to hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Link } from "react-router-dom";
 import s from "../css/home.module.css";
 import Title from "../components/Title";
+import Button from "../components/Button";
 
 import moon from "../images/moon.svg"
+import down from "../images/down.png"
 
 function Home() {
+  const feed = useRef();
+
+  const handleGoToFeedClick = (event) => {
+    feed.current.scrollIntoView({ behavior: "smooth" });
+  }
   return (
     <div className="main">
       {/* <Title title="Luna Home">
@@ -23,13 +30,14 @@ function Home() {
           This website contains a collection of functionalities which I thought were interesting, fun to implement and/or useful.
           It will be continuously updated. 
           </h3>
+          <button className={s.hero__downBtn} onClick={handleGoToFeedClick}>Go to feed <img src={down} alt={"down"} className={s.hero__downIcon}/></button>
         </div>
         <div className={s.hero__logo}>
           <img src={moon} alt={"logo"} className={s.hero__logoIcon}/>
         </div>
       </div>
       <div className={s.feed__container}>
-        <div className={s.feed}>
+        <div className={s.feed} ref={feed}>
           <div className={s.feed__item}>
             <div className={s.feed__itemHeader}><h2>Luna Spellcheck</h2></div>
             <div className={s.feed__itemBody}>One of the functionalities which I'm currently working on is a Papiamentu-Dutch Translator.
@@ -40,7 +48,6 @@ function Home() {
             <div className={s.feed__itemHeader}><h2>Luna Translate</h2></div>
             <div className={s.feed__itemBody}> Once obtaining enough data, this will be made.</div>
           </div>
-          
         </div>
       </div>
     </div>
