dfc9491fd48eca9373c3c449ed9c5abf7f6bdd73
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2022-06-28 22:05:09 +0200

    Add cool transition animation on hero logo hover

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,8 +2,25 @@ import React, { useEffect, useState, useRef, forwardRef } from 'react';
 import s from "../css/hero.module.css";
 import down from "../images/down.png"
 import moon from "../images/moon.svg"
+import star from "../images/star.png"
 
 function Hero(props) {
+
+  const [isLogoActive, setIsLogoActive] = useState(false);
+
+  const handleLogoMouseEnter = (event) => {
+    setIsLogoActive(true);
+  }
+
+  const handleLogoMouseLeave = (event) => {
+    setIsLogoActive(false);
+  }
+
+  const getConditionalclass = (cls, conditionalclass, condition) => {
+    return cls + " " + (condition ? conditionalclass : "")
+  }
+
+
 
   return (
     <div className={s.hero}>
@@ -16,8 +33,13 @@ function Hero(props) {
         </h3>
         <button className={s.hero__downBtn} onClick={props.handleGoToFeedClick}>{props.command} <img src={down} alt={"down"} className={s.hero__downIcon} /></button>
       </div>
-      <div className={s.hero__logo}>
-        <img src={moon} alt={"logo"} className={s.hero__logoIcon} />
+      <div className={getConditionalclass(s.hero__logo, s.active, isLogoActive)} onMouseEnter={handleLogoMouseEnter} onMouseLeave={handleLogoMouseLeave}>
+        <img src={moon} alt={"logo"} className={getConditionalclass(s.hero__logoIcon, s.active, isLogoActive)} />
+        <div>
+          {[...Array(4).keys()].map(x => (
+            <img src={star} alt={"logo"} className={getConditionalclass(s.hero__logoStar, s.active, isLogoActive)} />
+          ))}
+        </div>
       </div>
     </div>
   );
